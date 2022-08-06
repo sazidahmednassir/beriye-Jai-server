b05930da6cade39066c3cc5a01db78c323eb3d01
sazidahmednassir <sazidahmednassir@gmail.com>
2022-08-06 19:10:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const express=require('express');
+const cors= require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app= express()
+
+//! Warning: Do not use in production
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+  
+app.use(express.json())
+
+const port=process.env.PORT || 5000;
+
+
+const uri = "mongodb+srv://admin:cWNclgd0ns7WHOQM@cluster0.xrvxz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// app.get('/', async(req, res)=>{
+//     res.sendFile("F:/hero/servectg-server/index.html")
+// })
+
+async function run() {
+    try {
+      await client.connect();
+      const servicesCollection = client.db("servectg").collection("services");
+  
+      app.get("/service", async (req, res) => {
+        const services = await servicesCollection.find({}).toArray();
+        console.log(services);
+        res.send(services);
+
+         //* With try catch block
+
+    // app.post("/add-service", async (req, res) => {
+    //   try {
+    //     const data = req.body;
+    //     const result = await servicesCollection.insertOne(data);
+    //     res.send({ status: true, result: result });
+    //   } catch (error) {
+    //     res.send({ status: false, error });
+    //   }
+    // });
+
+        app.post("/add-service", async (req, res) => {
+          const data = req.body;
+          const result = await servicesCollection.insertOne(data);
+          res.send(result);
+        });
+
+        app.put("/update-service/:id", async (req, res) => {
+          const { id } = req.params;
+          const data = req.body;
+    
+          const filter = { _id: ObjectId(id) };
+          const updateDoc = { $set: data };
+          const option = { upsert: true };
+    
+          const result = await servicesCollection.updateOne(
+            filter,
+            updateDoc,
+            option
+          );
+    
+          res.send(result);
+        });
+      });
+
+      app.delete("/delete-service/:id", async (req, res) => {
+        const { id } = req.params;
+        const query = { _id: ObjectId(id) };
+        const result = await servicesCollection.deleteOne(query);
+  
+        res.send(result);
+      });
+
+    } finally {
+    }
+  }
+  run().catch(console.dir);
+
+  app.get("/dummy-route/user2", async (req, res) => {
+    const data = req.body;
+  
+    res.json(data);
+  });
+  
+  
+  app.get("/", async (req, res) => {
+    res.send("Hello");
+  });
+
+
+  app.get("/dummy-route/user", async (req, res) => {
+    const { name, age } = req.query;
+    console.log(name);
+    console.log(age);
+    res.json(name);
+  });
+
+
+  app.get("/user/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    res.json(id);
+  });
+  
+ 
+
+app.listen(port, ()=>{
+    console.log(`listening on ${port}`)
+})
